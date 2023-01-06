f0a7821262343d40fccbc03f3e22d99063b40e0b
Stephan van den Berkmortel <stephan.van.den.berkmortel@algebrakit.nl>
2023-01-06 11:04:26 +0100

    #7355: escape hashtag symbol

--- src/commands/math/basicSymbols.js
+++ src/commands/math/basicSymbols.js
@@ -307,6 +307,7 @@ var NonSymbolaSymbol = P(Symbol, function(_, super_) {
 LatexCmds['@'] = NonSymbolaSymbol;
 LatexCmds['&'] = bind(NonSymbolaSymbol, '\\&', '&amp;');
 LatexCmds['%'] = bind(NonSymbolaSymbol, '\\%', '%');
+LatexCmds['#'] = bind(VanillaSymbol, '\\#', '#');
 LatexCmds['permil'] = LatexCmds['permille'] = LatexCmds['‰'] = bind(NonSymbolaSymbol, '‰', '&#8240;');
 
 //the following are all Greek to me, but this helped a lot: http://www.ams.org/STIX/ion/stixsig03.html
